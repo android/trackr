e430e5a2c46094fa4fd8bb5d569d7ae1a3a279ce
Shailen Tuli <shailentuli@google.com>
2020-09-30 11:03:32 -0700

    Basic colors and theming
    Change-Id: Ic50e76a341e7ddac21a25fa58290a82318b34f3a

--- app/src/main/java/com/example/android/trackr/MainActivity.kt
+++ app/src/main/java/com/example/android/trackr/MainActivity.kt
@@ -27,7 +27,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
 
         // TODO(b/163065333): move to application.
         AndroidThreeTen.init(this)
