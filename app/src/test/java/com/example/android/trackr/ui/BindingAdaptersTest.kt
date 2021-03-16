6353c578201d6609b2bf12d8abfad21fdac6922c
Caren Chang <carenc@google.com>
2021-03-16 14:17:55 -0700

    Cleanup

--- app/src/test/java/com/example/android/trackr/ui/BindingAdaptersTest.kt
+++ app/src/test/java/com/example/android/trackr/ui/BindingAdaptersTest.kt
@@ -55,11 +55,6 @@ class BindingAdaptersTest {
     }
 
     @Test
-    fun exampleFailTest() {
-        assertThat(false).isTrue()
-    }
-
-    @Test
     fun showFormattedDueMessageOrDueDate_withNullDueDate() {
         showFormattedDueMessageOrDueDate(textView, null, fakeClock)
         assertThat(textView.text).isEqualTo("")
